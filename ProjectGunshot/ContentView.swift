e458116927648bfdd50bafb1985e3dc1d1080c1f
Jevon Mao <woodburyjevonmao@gmail.com>
2022-06-05 23:06:52 -0700

    Initial Commit

---
+++ ProjectGunshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectGunshot
+//
+//  Created by Jevon Mao on 6/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectGunshot/ProjectGunshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectGunshotApp.swift
+//  ProjectGunshot
+//
+//  Created by Jevon Mao on 6/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectGunshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
